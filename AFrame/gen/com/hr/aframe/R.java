59b95ec002c7e08e5ff83284af1d937bbf4e32e0
hrxiang <xianghairui@126.com>
2015-04-30 16:25:32 +0800

    4/30

--- AFrame/gen/com/hr/aframe/R.java
+++ AFrame/gen/com/hr/aframe/R.java
@@ -23,6 +23,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int list=0x7f090001;
         public static final int test=0x7f090000;
     }
     public static final class layout {
--- AFrame/src/com/hr/aframe/TestActivity.java
+++ AFrame/src/com/hr/aframe/TestActivity.java
@@ -1,11 +1,15 @@
 package com.hr.aframe;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import android.os.Handler;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.android.volley.Request.Method;
+import com.hr.aframe.adapter.TestAdapter;
 import com.hr.aframe.base.BaseActivity;
 import com.hr.aframe.base.ViewInject;
 import com.hr.aframe.bean.TestBean;
@@ -14,6 +18,9 @@ public class TestActivity extends BaseActivity {
 	private static final String url = "http://gc.ditu.aliyun.com/geocoding";
 	@ViewInject(R.id.test)
 	private ViewGroup mTestGroup;
+	@ViewInject(R.id.list)
+	private ListView mListView;
+
 	private Map<String, String> mRequestParams;
 	private Map<String, String> mRequestHeaders;
 
@@ -38,6 +45,18 @@ public class TestActivity extends BaseActivity {
 	@Override
 	protected void initDatas() {
 		// TODO Auto-generated method stub
+		List<String> list = new ArrayList<String>();
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		list.add("D");
+		list.add("E");
+		TestAdapter<String> adapter = new TestAdapter<String>(mContext, list);
+		mListView.setAdapter(adapter);
+
+		/***
+		 * 数据请求和数据缓存测试
+		 * */
 		mBaseGsonService.addRequestToQueue(mHandler, Method.GET, url,
 				mRequestParams, mRequestHeaders, TestBean.class, true);
 	}
